380ae334d79cc3192763bc8e9279be4eb91d49d2
Not-Daniel <73255853+Not-Daniel@users.noreply.github.com>
2020-10-22 15:51:55 +0200

    embed upadte
    dasd

--- main.js
+++ main.js
@@ -13,10 +13,20 @@ bot.on("message",msg=>{
     let args = msg.content.substring(prefix.length).split(" ");
     switch (args[0]) { 
         case 'request':
-            const content = msg.content;
-            const content2 = content.replace(/!request/,"");
-            msg.author.send("✅The request has been sent!✅");
-            msg.client.channels.cache.get('768789192951332874').send(`${content2}`);
+            //vary a struktura message
+            const usr = msg.author.username;
+            var content = msg.content;
+            var content = content.replace(/!request/,"");
+            var content = `${content} ${usr}`;
+            //konec varu 
+            //embed
+            const embed =  new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Request')
+                .setDescription(`${content}`)
+
+            msg.author.send("✅The request has been sent!✅");//napise odesilataly ✅The request has been sent!✅
+            msg.client.channels.cache.get('768807673801736213').send(embed);
         break;
     }
 });
